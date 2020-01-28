47fc1cd38a5a54ea086419c2f679db3122cf71b2
Koki Fukuda <ko.fu.dev@gmail.com>
2020-01-28 15:28:41 +0900

    Use multi process

--- generate.py
+++ generate.py
@@ -1,9 +1,20 @@
 import sys
+import concurrent.futures
+
 from PIL import Image
 import anvil
 import blockcolors
 
-def generate_256(region, region_x, region_z, offx, offz):
+def generate_256(*args):
+    region = args[0]
+    region_x = args[1]
+    region_z = args[2]
+    offx = args[3]
+    offz = args[4]
+    verbose = args[5]
+    if verbose:
+        print('generating chunk: ' + str(offx) + ', ' + str(offz))
+
     outimg = Image.new('RGBA', (16*16,16*16))
     outname = str(region_x * 2 + offx) + ',' + str(region_z * 2 + offz) + '.png'
     for chz in range(16):
@@ -55,15 +66,45 @@ def generate_256(region, region_x, region_z, offx, offz):
 
     outimg.save(outname)
 
-def main(region_dir, region_x, region_z):
-    region = anvil.Region.from_file(region_dir + '/r.' + str(region_x) + '.' + str(region_z) + '.mca')
-    for offx in range(2):
-        for offz in range(2):
-            generate_256(region, region_x, region_z, offx, offz)
+def main(region_dir, region_x, region_z, nproc, verbose):
+    if verbose:
+        print('loading region')
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=nproc) as executor:
+        region = anvil.Region.from_file(region_dir + '/r.' + str(region_x) + '.' + str(region_z) + '.mca')
+        for offx in range(2):
+            for offz in range(2):
+                executor.submit(generate_256, region, region_x, region_z, offx, offz, verbose)
+
+def print_usage():
+    print('usage: generate.py [--proc n_procs] [--verbose] region_dir region_x region_z')
 
 if __name__ == '__main__':
-    if len(sys.argv) < 4:
-        print('usage: generate.py region_dir region_x region_z')
-        exit()
-    else:
-        main(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+    nproc = 2
+    verbose = False
+    normal_args = []
+
+    i = 1
+    while i < len(sys.argv):
+        if sys.argv[i] == '--procs':
+            i += 1
+            if i < len(sys.argv):
+                nproc = int(sys.argv[i + 1])
+            else:
+                print_usage()
+                sys.exit(1)
+        elif sys.argv[i] == '--verbose':
+            verbose = True
+        elif sys.argv[i] == '--help':
+            print_usage()
+            sys.exit(0)
+        else:
+            normal_args.append(sys.argv[i])
+
+        i += 1
+
+    if len(normal_args) < 2:
+        print_usage()
+        sys.exit(1)
+
+    main(normal_args[0], int(normal_args[1]), int(normal_args[2]), nproc, verbose)
