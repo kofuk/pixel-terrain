1847ecf083503328b7be70b8b3260ee88aec2d7c
Koki Fukuda <ko.fu.dev@gmail.com>
2020-05-31 11:40:20 +0900

    Add target to compile only tests

--- cmake/test_utils.cmake
+++ cmake/test_utils.cmake
@@ -1,6 +1,8 @@
 enable_testing()
 find_package(Boost COMPONENTS unit_test_framework)
 
+add_custom_target(test_executable)
+
 function(add_boost_test target_name test_name)
   if(NOT Boost_FOUND)
     return()
@@ -11,6 +13,8 @@ function(add_boost_test target_name test_name)
   target_compile_definitions(${target_name} PRIVATE "BOOST_TEST_DYN_LINK=1")
   target_compile_definitions(${target_name} PRIVATE "BOOST_TEST_MAIN")
   target_link_libraries(${target_name} ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY})
+
+  add_dependencies(test_executable ${target_name})
 
   add_test(NAME ${test_name} COMMAND ${target_name})
 endfunction()
