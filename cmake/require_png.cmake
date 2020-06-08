f72d13694d75eb3157b7ba3eb36099c895c86206
Koki Fukuda <ko.fu.dev@gmail.com>
2020-06-08 16:34:59 +0900

    Fix MSVC dependency issue

--- cmake/require_png.cmake
+++ cmake/require_png.cmake
@@ -3,12 +3,14 @@ if(NOT PNG_FOUND)
   include(ExternalProject)
   ExternalProject_Add(png_project
     URL https://download.sourceforge.net/libpng/libpng-1.6.37.tar.xz
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${PROJECT_SOURCE_DIR}/external -DCMAKE_BUILD_TYPE=Release)
-  add_library(png IMPORTED STATIC)
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${PROJECT_SOURCE_DIR}/external -DCMAKE_BUILD_TYPE=Release
+    -DPNG_BUILD_ZLIB=NO -DZLIB_INCLUDE_DIR=${ZLIB_INCLUDE_DIRS}
+    -DZLIB_LIBRARY=${ZLIB_LIBRARY})
   if(ZLIB_USE_EXTERNAL_PROJECT)
-    add_dependencies(png zlib)
+    add_dependencies(png_project zlib_project)
   endif()
-  set_target_properties(png PROPERTIES
+  add_library(png IMPORTED STATIC)
+  set_target_properties(png_project PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES ${ZLIB_INCLUDE_DIRS}
     INTERFACE_LINK_LIBRARIES ${ZLIB_LIBRARIES})
 
