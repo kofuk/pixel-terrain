15d9838b8c74479580e21c11c64c5dafb2c3e231
Koki Fukuda <ko.fu.dev@gmail.com>
2020-12-05 19:01:44 +0900

    Improve loading block color data

--- src/image/blocks.cc
+++ src/image/blocks.cc
@@ -8,8 +8,8 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -37,16 +37,17 @@ namespace pixel_terrain::image {
     void init_block_list() {
         std::size_t off = 0;
         for (;;) {
-            std::size_t len =
-                std::strlen(reinterpret_cast<char const *>(block_colors_data) + off);
+            std::size_t len = std::strlen(
+                reinterpret_cast<char const *>(block_colors_data) + off);
             if (len == 0) {
                 break;
             }
             std::string_view block_name(
                 reinterpret_cast<char const *>(block_colors_data) + off, len);
             off += len + 1;
-            colors[block_name] =
-                *reinterpret_cast<std::uint32_t const *>(block_colors_data + off);
+            std::uint32_t color;
+            std::memcpy(&color, block_colors_data + off, sizeof(std::uint32_t));
+            colors[block_name] = color;
             off += 4;
         }
     }
